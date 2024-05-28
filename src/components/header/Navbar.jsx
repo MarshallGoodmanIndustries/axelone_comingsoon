7794945b0bf9b38147cf25bd61a0174cb7cc6ca0
sleek-ennju <ihenachochukwudi@gmail.com>
2024-05-28 20:14:52 +0100

    replaced old logo with new

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from '../../assets/images/logobgremove.png';
+import logo from '../../assets/images/fyndah_logo.png';
 
 
 function Navbar() {
@@ -8,7 +8,7 @@ function Navbar() {
     }
   return (
     <nav className='relative z-50 bg-white flex items-center justify-between px-4 w-full h-[16dvh] sm:px-5 md:px-6 lg:px-16 '>
-        <div className='max-w-32 h-auto transform -translate-x-3'>
+        <div className='max-w-[6rem] sm:max-w-[8rem] md:max-w-[10rem] h-auto  transform -translate-x-3 '>
             <img src={logo} className='w-full h-full object-cover' alt="company logo" />
         </div>
         <button onClick={scrollDown} className="bg-accent text-primary font-poppins text-lg md:text-xl rounded-lg py-2 px-4 capitalize font-medium">Join waitlist</button>
