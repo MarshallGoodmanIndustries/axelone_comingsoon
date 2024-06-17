56612bfe912e129965a9974427439894f4aa3664
sleek-ennju <ihenachochukwudi@gmail.com>
2024-06-17 14:13:05 +0100

    made button clickable

--- src/components/main/Explore.jsx
+++ src/components/main/Explore.jsx
@@ -4,7 +4,7 @@ const Explore = () => {
   return (
     <section className="relative flex justify-center items-center flex-col gap-4 w-full py-24 px-4 sm:px-5 md:px-6 lg:px-16 md:p-8 bg-[#0a1128]">
         <img src={curvedLines} className="absolute top-0 left-0 w-full h-full object-cover opacity-30" alt="curved lines" />
-        <button className="bg-accent hover:bg-accentDark font-poppins font-normal text-primary text-lg md:text-xl px-8 py-2 rounded-lg transition-all duration-300"><a href="https://fyndah.com/" target="_blank" rel="noopener noreferrer">Explore Fyndah</a></button>
+        <button className=" relative z-10 bg-accent hover:bg-accentDark font-poppins font-normal text-primary text-lg md:text-xl px-8 py-2 rounded-lg transition-all duration-300 cursor-pointer"><a href="https://fyndah.com/" target="_blank">Explore Fyndah</a></button>
     </section>
   )
 }
